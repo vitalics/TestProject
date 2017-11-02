d9f665fd0706d49260165cfb578816fadfc838af
Vitali_Haradkou <Vitali_Haradkou@epam.com>
2017-11-02 11:07:00 +0300

    selenium-new(implement): html report selenium-new(fix): instruction for packages selenium-new(fix): remove duplicate dependencies

--- seleniumjs/protractor.conf.js
+++ seleniumjs/protractor.conf.js
@@ -1,6 +1,7 @@
 let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 // https://github.com/angular/protractor/issues/1451
 require('protractor/built/logger').Logger.logLevel = 1;
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 
 exports.config = {
     framework: 'jasmine2',
@@ -17,17 +18,23 @@ exports.config = {
     capabilities: {
         browserName: 'chrome',
         'chromeOptions': {
-            args: ['--test-type']
+            args: ['--headless','--test-type']
         }
     },
     onPrepare: function () {
-        jasmine.getEnv().addReporter(new SpecReporter({
-            spec: {
-                displayStacktrace: true
-            },
-            summary: {
-                displayDuration: false
-            }
-        }));
+        jasmine.getEnv().addReporter(new SpecReporter(
+            {
+                spec: {
+                    displayStacktrace: true,
+                },
+                summary: {
+                    displayDuration: false
+                }
+            }));
+        jasmine.getEnv().addReporter(new Jasmine2HtmlReporter({
+            savePath: './spec/reports/',
+            cleanDestination: true,
+            fileName: 'index',
+        }))
     }
 };
