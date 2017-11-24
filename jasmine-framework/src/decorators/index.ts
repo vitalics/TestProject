8718818e9ca40cbbccb06916f686fcf645497cfc
Vitali_Haradkou <Vitali_Haradkou@epam.com>
2017-11-24 17:36:00 +0300

    framework(implement): register decorator

--- jasmine-framework/src/decorators/index.ts
+++ jasmine-framework/src/decorators/index.ts
@@ -1,3 +1,4 @@
 export { describe } from "./describe.decorator";
 export { it } from "./it.decorator";
 export { Test } from "./test.decorator";
+export { register } from './register.decorator'
--- jasmine-framework/src/decorators/register.decorator.ts
+++ jasmine-framework/src/decorators/register.decorator.ts
@@ -1,16 +1,13 @@
-import "reflect-metadata";
 import { Compiler } from "../compiler/compiler";
 // let compiler: Compiler = new Compiler();
-export function it(description: string): any {
+export function register(extraParams?: any): any {
     return (target: any, propertyKey: string, descriptor: PropertyDescriptor): any => {
         var oldValue = descriptor.value;
-        // console.log(oldValue.toSource());
 
         descriptor.value = function (...args: any[]) {
             console.log(`Calling "${propertyKey}" with`, arguments, target);
-            //let value = oldValue.apply(null, [arguments[1], arguments[0]]);
 
-            Compiler.compileIt(oldValue, description, args);
+            Compiler.compileFunction(oldValue, propertyKey);
             // return oldValue.apply(this, args);
         }
         return descriptor;
